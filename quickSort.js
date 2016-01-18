6585159787ca5e9c3271d795d92921a6a841a03f
lianer <lianer@live.cn>
2016-01-18 23:49:07 +0800

    add quickSort

---
+++ quickSort.js
@@ -0,0 +1,101 @@
+var arr=[];
+
+var count={
+    quick: 0,
+    normal: 0
+};
+
+function quickSort(arr, desc) {
+    if(arr.length<2){
+        return arr;
+    }
+
+    var base=arr[0], small=[], big=[];
+
+    for (var i = 1, len=arr.length, n; i<len; i++) {
+        count.quick++;
+        n=arr[i];
+        if(n<base){
+            small.push(n);
+        }
+        else{
+            big.push(n);
+        }
+    };
+
+    if(desc){
+        return quickSort(big, true).concat(base).concat(quickSort(small, true));
+    }
+    else{
+        return quickSort(small).concat(base).concat(quickSort(big));
+    }
+}
+
+function normalSort(arr, desc) {
+    for (var i = 0, len=arr.length, temp; i < len; i++) {
+        for (var j = i; j < len; j++) {
+            count.normal++;
+            if(desc){
+                if(arr[j]>arr[i]){
+                    temp=arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=temp;
+                }
+            }
+            else{
+                if(arr[j]<arr[i]){
+                    temp=arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=temp;
+                }
+            }
+        };
+    };
+    return arr;
+}
+
+function createRandom(length, max, min) {
+    var arr=[];
+    for (var i = 0, r; i < length; i++) {
+        r=Math.random()*(max-min)+min;
+        arr.push(r);
+    }
+    return arr;
+}
+
+// quickSort(createRandom(10, 1, 10));
+
+test();
+
+function test() {
+    var length=2e4;
+    console.log("start...");
+    console.log("length: " + length);
+    console.log("-----------");
+
+    setTimeout(function () {
+        arr=createRandom(length, 0, 100);
+        arr=testQuick(arr, true);
+
+        setTimeout(function () {
+            arr=createRandom(length, 0, 100);
+            arr=testNormal(arr, true);
+
+            console.log("-----------");
+            console.log(count);
+            console.log("-----------");
+        }, 300);
+    }, 300);
+
+    function testQuick(arr, time) {
+        if(time){ var st=new Date(); }
+        quickSort(arr);
+        if(time){ console.log("quick: " + (new Date() - st) + "ms"); }
+    }
+
+    function testNormal(arr, time) {
+        if(time){ var st=new Date(); }
+        normalSort(arr);
+        if(time){ console.log("normal: " + (new Date() - st) + "ms"); }
+    }
+}
