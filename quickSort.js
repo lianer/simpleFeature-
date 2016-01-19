49e16dee5f585f8c30c9eda264ca06ce54df02bf
Lou Yunlong <lianer@live.cn>
2016-01-19 10:23:57 +0800

    update quickSort

--- quickSort.js
+++ quickSort.js
@@ -3,26 +3,28 @@
  * 记录：
  *     length: 2,000
  *     quick:  7ms         25,839次循环
- *     normal: 8ms         2,001,000次循环
+ *     bubble: 8ms         1,999,000次循环
  * 
  *     length: 20,000
  *     quick:  25ms        330,574次循环
- *     normal: 952ms       约2亿次循环
+ *     bubble: 952ms       约2亿次循环
  * 
  *     length: 200,000
  *     quick:  206ms       4,164,458次循环
- *     normal: 99,202ms    等了许久，洗了个脸回来终于好了，约200亿次循环
+ *     bubble: 99,202ms    等了许久，洗了个脸回来终于好了，约200亿次循环
  * 结论：
  *     数组长度越大快速排序法越优于普通排序法，有奇效
  */
 
 var arr=[];
 
+// 循环次数统计
 var count={
     quick: 0,
-    normal: 0
+    bubble: 0
 };
 
+// 快速排序
 function quickSort(arr, desc) {
     if(arr.length<2){
         return arr;
@@ -49,10 +51,11 @@ function quickSort(arr, desc) {
     }
 }
 
-function normalSort(arr, desc) {
+// 冒泡排序
+function bubbleSort(arr, desc) {
     for (var i = 0, len=arr.length, temp; i < len; i++) {
-        for (var j = i; j < len; j++) {
-            count.normal++;
+        for (var j = i + 1; j < len; j++) {
+            count.bubble++;
             if(desc){
                 if(arr[j]>arr[i]){
                     temp=arr[j];
@@ -72,6 +75,7 @@ function normalSort(arr, desc) {
     return arr;
 }
 
+// 
 function createRandom(length, max, min) {
     var arr=[];
     for (var i = 0, r; i < length; i++) {
@@ -86,7 +90,9 @@ function createRandom(length, max, min) {
 test();
 
 function test() {
+    // 随机数组长度
     var length=2e4;
+
     console.log("start...");
     console.log("length: " + length);
     console.log("-----------");
@@ -97,7 +103,7 @@ function test() {
 
         setTimeout(function () {
             arr=createRandom(length, 0, 100);
-            testNormal(arr, true);
+            testBubble(arr, true);
 
             console.log("-----------");
             console.log(count);
@@ -108,12 +114,12 @@ function test() {
     function testQuick(arr) {
         var st=new Date();
         quickSort(arr);
-        if(time){ console.log("quick: " + (new Date() - st) + "ms"); }
+        console.log("quick: " + (new Date() - st) + "ms");
     }
 
-    function testNormal(arr) {
+    function testBubble(arr) {
         var st=new Date();
-        normalSort(arr);
-        console.log("normal: " + (new Date() - st) + "ms");
+        bubbleSort(arr);
+        console.log("bubble: " + (new Date() - st) + "ms");
     }
 }
